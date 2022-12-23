3d453de919c502b976f7e56ac7dd3d045e2768a7
Hakan Baran <hakanbaran.developer@gmail.com>
2022-12-23 22:03:11 +0300

    Interface design was done. Components are defined in code.

---
+++ Week-3-Homework-1-Closure/FirstVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week-3-Homework-1-Closure
+//
+//  Created by Hakan Baran on 23.12.2022.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var surnameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func changeClicked(_ sender: Any) {
+    }
+    
+}
+
