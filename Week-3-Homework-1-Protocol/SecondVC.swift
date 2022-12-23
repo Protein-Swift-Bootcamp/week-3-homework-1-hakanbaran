6cb5e3b7a1ee02b6b9a867d63a67f4e8d03d8208
Hakan Baran <hakanbaran.developer@gmail.com>
2022-12-23 20:05:48 +0300

    Interface design was done. Components are defined in code.

---
+++ Week-3-Homework-1-Protocol/SecondVC.swift
@@ -0,0 +1,27 @@
+//
+//  SecondVC.swift
+//  Week-3-Homework-1-Protocol
+//
+//  Created by Hakan Baran on 23.12.2022.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var surnameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
+    
+
+}
