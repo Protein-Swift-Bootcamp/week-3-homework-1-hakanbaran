1da9891b597d07a8ac2129c153b28f11773fc373
Hakan Baran <hakanbaran.developer@gmail.com>
2022-12-23 03:19:31 +0300

    Interface design was done. Components are defined in code.

---
+++ Week-3-Homework-1-Notification-Center/SecondVC.swift
@@ -0,0 +1,29 @@
+//
+//  SecondVC.swift
+//  Week-3-Homework-1-Notification-Center
+//
+//  Created by Hakan Baran on 23.12.2022.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var surnameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+    
+
+}
