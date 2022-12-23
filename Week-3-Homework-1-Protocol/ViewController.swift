5943d0502aff4229636b5d9d7d0f781c487f31b6
Hakan Baran <hakanbaran.developer@gmail.com>
2022-12-23 19:59:19 +0300

    Protocol- Project was created.

---
+++ Week-3-Homework-1-Protocol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week-3-Homework-1-Protocol
+//
+//  Created by Hakan Baran on 23.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
